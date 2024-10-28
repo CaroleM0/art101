7592d3cf458716d760042680e67ec656df33bdde
CaroleM0 <kqingmo1@gmail.com>
2024-10-28 13:50:37 -0700

    11

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -4,20 +4,24 @@
 // sortUserName - a function that takes user inut and sort the letters
 // of their name
 function sortUserName() {
-    var userName = window.prompt("Hi. Please tell me your name so I can finx it.");
+    var userName = window.prompt("Hi. Can I have your name?");
     console.log("userName =", userName);
+
     // split string to array
     var nameArray = userName.split('');
     console.log("nameArray =", nameArray);
-        // sort the array
-        var nameArraySort = nameArray.sort();
-        console.log("nameAarraysort =", nameArraysort);
-        // join array back to a string
-        var nameSorted = nameArraySort.join('');
-        console.log("nameSorted =", nameSorted);
-        // Note that I chould have done the above lines as a single line:
-        //   userName.toLower().split("").sort().join("")
-        return nameSorted;
+
+    // sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameAarraysort =", nameArraysort);
+
+    // join array back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+
+    // Note that I chould have done the above lines as a single line:
+    //   userName.toLower().split("").sort().join("")
+    return nameSorted;
 }
 
 
