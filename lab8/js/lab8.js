b15f3bd6e95ca668e54c6efa68473e6ce69adf86
CaroleM0 <kqingmo1@gmail.com>
2024-10-31 17:43:15 -0700

    lab8

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -1,30 +1,23 @@
 // Author: Kaiqing Mo
 // Date:10/28/2024
 
-// sortUserName - a function that takes user inut and sort the letters
-// of their name
-function sortUserName() {
-    var userName = window.prompt("Hi. Can I have your name?");
-    console.log("userName =", userName);
-
-    // split string to array
-    var nameArray = userName.split('');
-    console.log("nameArray =", nameArray);
-
-    // sort the array
-    var nameArraySort = nameArray.sort();
-    console.log("nameAarraysort =", nameArraySort);
-
-    // join array back to a string
-    var nameSorted = nameArraySort.join('');
-    console.log("nameSorted =", nameSorted);
-
-    // Note that I chould have done the above lines as a single line:
-    //   userName.toLower().split("").sort().join("")
-    return nameSorted;
+function isEven(x){
+    return (x % 2 == 0);
 }
 
+// test function
+console.log("Is 1 even?", isEven(1));
+console.log("Is 2 even?", isEven(2));
 
-// ouput
-document.writeln("Oh hey, your sortnname:",
-        sortUserName(),"</br");
+array = [100, 81, 4, 16, 42, 144, 10000]
+console.log("My array", array);
+
+var result = array.map(isEven);
+// should return [ture, false, ture, true, ture, ture]
+console.log("Test of evenness of array:", result);
+
+var result = array.map(function(x){
+    return x ** 0.5;
+})
+// should return [10, 9, 2, 4, 6.48074069840786, 12, 100]
+console.log("Squareroot of array:", result);
